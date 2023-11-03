5fd1c3095e7a65b02b7058da218cbbd58b54d2ec
fahizoune10 <fahizoune10@gmail.com>
2023-11-03 18:17:32 +0100

    Api helpers for CRUD operations, and filtering, Sorting, selecting and searching features

---
+++ src/api/utils/ApiError.ts
@@ -0,0 +1,17 @@
+//@desc   Error class handler
+class ApiError extends Error {
+  public statusCode: number;
+
+  public status: string;
+
+  public isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = String(statusCode).startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+  }
+}
+
+export default ApiError;
---
+++ src/api/utils/ServicHandler.ts
@@ -0,0 +1,109 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response, NextFunction } from 'express';
+import { prisma } from '../db';
+import {
+  convertSortToObject,
+  convertFieldsToObject,
+  convertKeywordsToObject,
+  convertFiltersToObject,
+  pagingsObject,
+} from './Helpers';
+
+export const createOne: any =
+  (model: keyof PrismaClient) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const name = String(model).charAt(0).toUpperCase() + String(model).slice(1);
+    const dynamicModel = prisma[model] as any;
+    const data = await dynamicModel.create({ data: req.body });
+    return res.status(201).json({ msg: `${name} created succesfully`, data });
+  };
+
+export const getOne: any =
+  (model: keyof PrismaClient) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const dynamicModel = prisma[model] as any;
+    const name = String(model).charAt(0).toUpperCase() + String(model).slice(1);
+
+    const data = await dynamicModel.findUnique({ where: { id } });
+    if (!data) return res.status(404).json({ error: `${name} not found!!` });
+
+    return res
+      .status(200)
+      .json({ message: `${name} returned succesfully`, data });
+  };
+
+export const updateOne: any =
+  (model: keyof PrismaClient) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const dynamicModel = prisma[model] as any;
+    const name = String(model).charAt(0).toUpperCase() + String(model).slice(1);
+
+    const data = await dynamicModel.findUnique({ where: { id } });
+    if (!data) return res.status(404).json({ error: `${name} not found!!` });
+
+    await dynamicModel.update({ where: { id }, data: req.body });
+
+    return res
+      .status(200)
+      .json({ message: `${name} updated succesfully`, data });
+  };
+export const deleteOne: any =
+  (model: keyof PrismaClient) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const dynamicModel = prisma[model] as any;
+    const name = String(model).charAt(0).toUpperCase() + String(model).slice(1);
+
+    const data = await dynamicModel.findUnique({ where: { id } });
+    if (!data) return res.status(404).json({ error: `${name} not found!!` });
+
+    await dynamicModel.delete({ where: { id } });
+
+    return res.status(204);
+  };
+
+export const getAll: any =
+  (model: keyof PrismaClient) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { page, limit, fields, sort, keywords, ...filters } = req.query;
+    const name = String(model);
+    const dynamicModel = prisma[model] as any;
+    const count = await dynamicModel.count();
+    const { skip, take, paging } = await pagingsObject(count, req.query);
+    // Apply filters
+    let data = await dynamicModel.findMany({
+      where: convertFiltersToObject(filters, String(model)),
+    });
+
+    // Apply fields selection
+    if (fields) {
+      data = await dynamicModel.findMany({
+        select: convertFieldsToObject(String(fields)),
+      });
+    }
+
+    // Apply sorting
+    if (sort) {
+      data = await dynamicModel.findMany({
+        orderBy: convertSortToObject(String(sort)),
+      });
+    }
+
+    // Apply search
+    if (keywords) {
+      data = await dynamicModel.findMany({
+        wher: convertKeywordsToObject(String(keywords)),
+      });
+    }
+
+    // Apply pagination
+    data = await dynamicModel.findMany({ skip, take });
+
+    return res.status(200).json({
+      message: `${name}(s) returned successfully`,
+      paging,
+      data,
+    });
+  };
