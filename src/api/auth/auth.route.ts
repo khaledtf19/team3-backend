84ae9ca32ad1b0d270c29c3a672faa742ea349bd
Khaledtf Labeb <khaledtf19@gmail.com>
2023-11-10 22:22:48 +0200

    /register logic

--- src/api/auth/auth.route.ts
+++ src/api/auth/auth.route.ts
@@ -1,16 +1,59 @@
 import express from "express";
 import { validate } from "../../middleware/validator";
-import { registerRequestSchema } from "./auth.validate";
+import { registerRequestSchema, loginRequestSchema } from "./auth.validate";
 import { z } from "zod";
+import { hash } from "bcrypt";
+import { prisma } from "../../db";
 
 const router = express.Router();
 
 router.post<{}, {}, z.infer<typeof registerRequestSchema>>(
   "/register",
   validate(z.object({ body: registerRequestSchema })),
+  async (req, res) => {
+    const oldUser = await prisma.user.findFirst({
+      where: { email: req.body.email },
+    });
+    if (oldUser?.id) {
+      return res.status(400).json({ error: "This email has been used before" });
+    }
+
+    const hashedPassword = await hash(req.body.password, 10);
+
+    if (req.body.role === "TEACHER") {
+      await prisma.teacher.create({
+        data: {
+          user: {
+            create: {
+              fullName: req.body.fullName,
+              email: req.body.email,
+              password: hashedPassword,
+            },
+          },
+        },
+      });
+    } else {
+      await prisma.student.create({
+        data: {
+          user: {
+            create: {
+              fullName: req.body.fullName,
+              email: req.body.email,
+              password: hashedPassword,
+            },
+          },
+        },
+      })
+    }
+    res.status(200).json({ message: "User created successfully" });
+  },
+);
+
+router.post<{}, {}, z.infer<typeof loginRequestSchema>>(
+  "/login",
+  validate(z.object({ body: loginRequestSchema })),
   (req, res) => {
-    
-    res.status(200).json(req.body);
+    res.status(200).json({});
   },
 );
 
--- src/api/auth/auth.validate.ts
+++ src/api/auth/auth.validate.ts
@@ -2,11 +2,20 @@ import { z } from "zod";
 
 
 export const registerRequestSchema = z.object({
-    name: z.string().min(3).max(100),
+    fullName: z.string().min(3).max(100),
     email: z.string().email(),
-    password: z.string(),
+    password: z.string().min(8).max(255),
+    confirmPassword:z.string().min(8).max(255),
     address: z.string().optional(),
     city: z.string().optional(),
     country: z.string().optional(),
     role: z.enum(["TEACHER","STUDENT"]).optional()
- });
+ }).refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords don't match",
+        path: ["confirm"],
+    });
+
+export const loginRequestSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(255)
+})
